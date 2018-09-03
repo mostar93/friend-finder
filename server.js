c8c49ec0f1fed51e4c04c0bd90d67df4dd74befd
mostar <mominahmad93@gmail.com>
2018-09-03 16:26:27 +0200

    yep

---
+++ server.js
@@ -0,0 +1,24 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+var chartApiRoutes = require("./app/routing/apiRoutes");
+var chartHTMLRoutes = require("./app/routing/htmlRoutes");
+
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+
+app.use("/", chartHTMLRoutes);
+app.use("/survey", chartHTMLRoutes);
+
+//START SERVER
+// ====================================================================
+app.listen(PORT, function(){
+    console.log("App listening on PORT " + PORT);
+});
